789e7c8ada46b2dd92be4d61d836c5b4819d00ac
Karla Garcia <karla.garcia@wizeline.com>
2019-07-02 21:36:22 -0500

    Add algorithm for sorting

--- api/middleware/middleware.go
+++ api/middleware/middleware.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+	"sort"
 	"strconv"
 	"strings"
 
@@ -20,7 +21,7 @@ type Queue struct {
 }
 
 // Que declaration
-var Que []string
+var Que []*Queue
 
 // Repository should implement common methods
 type Repository interface {
@@ -42,6 +43,7 @@ func (q *Queue) Read() []*Queue {
 	for scanner.Scan() {
 		count++
 		if scanner.Text() == "" {
+			fmt.Println("PUSHED", tmp)
 			count = 0
 			final = append(final, tmp)
 			tmp = &Queue{}
@@ -57,6 +59,7 @@ func (q *Queue) Read() []*Queue {
 		case 3:
 			tmp.Priority, _ = strconv.Atoi(strings.Split(scanner.Text(), ":")[1])
 		}
+		fmt.Println(tmp)
 		fmt.Println("IN", scanner.Text())
 	}
 
@@ -84,6 +87,29 @@ func MockQueue() []*Queue {
 	}
 }
 
+func SortedAppend(que []*Queue, request *Queue) []*Queue {
+	index := sort.Search(len(que), func(i int) bool {
+		return que[i].Weight > request.Weight && que[i].Priority > request.Priority
+	})
+	que = append(que, &Queue{})
+	copy(que[index+1:], que[index:])
+	que[index] = request
+	return que
+}
+
+func customSorting(queue []*Queue) []*Queue {
+	sort.Slice(queue, func(i, j int) bool {
+		if queue[i].Weight < queue[j].Weight {
+			return true
+		}
+		if queue[i].Weight > queue[j].Weight {
+			return false
+		}
+		return queue[i].Priority < queue[j].Priority
+	})
+	return queue
+}
+
 // ProxyMiddleware should queue our incoming requests
 func ProxyMiddleware(c iris.Context) {
 	domain := c.GetHeader("domain")
@@ -94,14 +120,12 @@ func ProxyMiddleware(c iris.Context) {
 	var repo Repository
 	repo = &Queue{}
 	fmt.Println("FROM HEADER", domain)
+	// Method 1: Insert and then sort by Weight and Priority
 	for _, row := range repo.Read() {
 		fmt.Println("FROM SOURCE", row.Domain)
-
-		//  ALGORITHM HERE...
-		//  USE QUE
-
+		Que = append(Que, row)
 	}
-	Que = append(Que, domain)
-
+	customSorting(Que)
+	fmt.Println("QUEUE", Que)
 	c.Next()
 }
